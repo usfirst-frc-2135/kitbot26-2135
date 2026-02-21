cb486b8a161e4ec6abf2b057bb038ee28bdc5d13
iman-sagarwala <isagarwala.28@presentationhs.org>
2026-02-21 09:51:13 -0800

    slowing motor speeds for a more accurate auto

--- src/main/java/frc/robot/commands/OutpostAuto.java
+++ src/main/java/frc/robot/commands/OutpostAuto.java
@@ -8,10 +8,11 @@ public class OutpostAuto extends SequentialCommandGroup
 {
     public OutpostAuto(CANDriveSubsystem drivesystem, CANFuelSubsystem fuelsystem)
     {
-        addCommands(new AutoDrive(drivesystem, 0.5, 0).withTimeout(1.5), new AutoDrive(drivesystem, 0, -0.75).withTimeout(0.5),
-                new AutoDrive(drivesystem, -0.75, 0).withTimeout(2.15), new AutoDrive(drivesystem, 0, -0.75).withTimeout(0.42),
-                new AutoDrive(drivesystem, -0.75, 0).withTimeout(1.35), new AutoDrive(drivesystem, 0, 0).withTimeout(1.5),
-                new AutoDrive(drivesystem, 0.75, 0).withTimeout(0.25));
+        addCommands(new AutoDrive(drivesystem, 0.5, 0).withTimeout(1.5), new AutoDrive(drivesystem, 0, -0.6).withTimeout(1.11),
+                new AutoDrive(drivesystem, -0.6, 0).withTimeout(3.37), new AutoDrive(drivesystem, 0, -0.6).withTimeout(1.11));
+        // new AutoDrive(drivesystem, -0.75, 0).withTimeout(2.17), new AutoDrive(drivesystem, 0, -0.75).withTimeout(0.53),
+        // new AutoDrive(drivesystem, -0.75, 0).withTimeout(1.4), new AutoDrive(drivesystem, 0, 0).withTimeout(1.5),
+        // new AutoDrive(drivesystem, 0.75, 0).withTimeout(0.25));
         // new ExampleAuto(drivesystem, fuelsystem)        //new AutoDrive(drivesystem,0,0.75).withTimeout(0.53)
         //new AutoDrive(drivesystem,0.75,0).withTimeout(1.1);
         //new AutoDrive(drivesystem,0,0.75).withTimeout(0.75);
